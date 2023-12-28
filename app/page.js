581d927e9b7c0e93e17869700e7fda07fcf7f62f
Lemar <lemarcanete4@gmail.com>
2023-12-28 23:47:52 +0800

    first push

--- app/page.js
+++ app/page.js
@@ -1,113 +1,116 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
+import axios from 'axios';
+
+const encodedParams = new URLSearchParams();
+
+const isValidIpAddress = (ip) => {
+  const ipAddressRegex = /^(\d{1,3}\.){0,3}\d{0,3}$/;
+  return ipAddressRegex.test(ip);
+};
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [address, setAddress] = useState("");
+    const [data, setData] = useState({});
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const handleInputChange = (e) => {
+        const inputValue = e.target.value;
+        setAddress(inputValue);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        if (!isValidIpAddress(inputValue)) {
+        setError("Please enter a valid IP address");
+        } else {
+        setError("");
+        }
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const handleSubmit = async () => {
+        if (!isValidIpAddress(address)) {
+            setError("Please enter a valid IP address");
+            return;
+        }
+       
+        setIsLoading(true);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        encodedParams.set('ip', address);
+        encodedParams.set('reverse-lookup', 'checked');
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        const options = {
+            method: 'POST',
+            url: 'https://community-neutrino-ip-info.p.rapidapi.com/ip-info',
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded',
+              'X-RapidAPI-Key': '20e5f0ded7mshabe3c54e7f19ce6p1ac5e6jsnb3c962c3081a',
+              'X-RapidAPI-Host': 'community-neutrino-ip-info.p.rapidapi.com'
+            },
+            data: encodedParams,
+        };
+        try {
+            const response = await axios.request(options);
+            setData(response.data);
+            console.log(response.data)
+            setIsLoading(false)
+            setError("");
+            document.getElementById('result-section').scrollIntoView({
+                behavior: 'smooth',
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <main className='container-fluid'>
+            <div className="h-screen border bg-no-repeat bg-cover bg-[url('https://images.pexels.com/photos/1203808/pexels-photo-1203808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')]">
+                <div className='mx-auto md:w-96 rounded p-3 w-72 bg-slate-200 shadow-lg mt-80'>
+                    <p className=''>Enter IP Address: 49.148.245.173</p>
+                    <div className="md:flex">
+                    <input
+                        type="text"
+                        className={`w-full border border-slate-300 rounded-md text-md shadow-sm placeholder-slate-400 focus:outline-none p-2 ${
+                        !isValidIpAddress(address) ? 'border-red-500' : ''
+                        } focus:border-sky-500`}
+                        onChange={handleInputChange}
+                        placeholder='192.158.1.38'
+                    />
+                    <button className='rounded-full bg-sky-500 hover:bg-sky-700 py-2 px-6 ms-3 mt-4 md:mt-0' onClick={handleSubmit}>Enter</button>
+                    </div>
+                    <span className='block text-red-500'>{error}</span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5 p-5 h-screen md:text-center" id='result=section'>
+                <div className=" bg-green-100 rounded-md p-3 md:pt-32 md:text-2xl font-medium shadow-xl hover:bg-white hover:shadow-none cursor-pointer ">
+                    <p className=''>Country: {data.country}</p>
+                    <p className=''>Region: {data.region}</p>
+                    <p className=''>City: {data.city}</p>
+                </div>
+                <div className=" bg-red-100 rounded-md p-3 md:pt-32 md:text-2xl font-medium shadow-xl hover:bg-white hover:shadow-none cursor-pointer">
+                    <p className=''>Latitude: {data.latitude}</p>
+                    <p className=''>Latitude: {data.longitude}</p>
+                    <p className=''>hostname: {data.hostname}</p>
+                    <p className=''>Continent Code: {data['host-domain']}</p>
+                    <p className=''>IP Address: {data.ip}</p>
+                </div>
+                <div className=" bg-blue-100 rounded-md p-3 md:pt-32 md:text-2xl font-medium shadow-xl hover:bg-white hover:shadow-none cursor-pointer">
+                    <p className=''>Continent Code: {data['continent-code']}</p>
+                    <p className=''>Continent Code: {data['country-code']}</p>
+                    <p className=''>Currency: {data['currency-code']}</p>
+                    <p className=''>Continent Code: {data['continent-code']}</p>
+                    <p className=''>Continent Code: {data['region-code']}</p>
+                </div>
+                <div className=" bg-yellow-100 rounded-md p-3 md:pt-32 md:text-2xl font-medium shadow-xl hover:bg-white hover:shadow-none cursor-pointer">
+                    <p className=''>Abbr: {data.timezone && data.timezone.abbr}</p> 
+                    <p className=''>Date: {data.timezone && data.timezone.date}</p>
+                    <p className=''>Id: {data.timezone && data.timezone.id}</p>
+                    <p className=''>Name: {data.timezone && data.timezone.name}</p>
+                    <p className=''>Offset: {data.timezone && data.timezone.offset}</p>
+                    <p className=''>Time: {data.timezone && data.timezone.time}</p>
+                </div>
+            </div>   
+        </main>
+    );
 }
